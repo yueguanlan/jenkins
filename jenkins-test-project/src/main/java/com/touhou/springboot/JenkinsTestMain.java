dc9230febc2baae91d4d1f6d1de3340cde6084b1
zhaoyuan <zhaoyuan@gmail.com>
2020-12-22 16:24:50 +0800

    提交jenkins

---
+++ jenkins-test-project/src/main/java/com/touhou/springboot/JenkinsTestMain.java
@@ -0,0 +1,12 @@
+package com.touhou.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestMain {
+
+    public static void main(String[] args){
+        SpringApplication.run(JenkinsTestMain.class, args);
+    }
+}
---
+++ jenkins-test-project/src/main/java/com/touhou/springboot/controller/JenkinsController.java
@@ -0,0 +1,17 @@
+package com.touhou.springboot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    private static final Logger log = LoggerFactory.getLogger(JenkinsController.class);
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+}
